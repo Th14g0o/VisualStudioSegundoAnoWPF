2b9621a9bef6c7217760d0bcf7a1d97f263618f2
Th14g0o <thiago.gonsalves@escolar.ifrn.edu.br>
2022-10-22 13:33:52 -0300

    Organizado

---
+++ aulas/aula001/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace aula001
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void inserir_Click(object sender, RoutedEventArgs e)
+        {
+            contato info = new contato(nomeuser.Text, foneuser.Text, emailuser.Text);
+            contatoslista.Items.Add(info);
+        }
+
+        private void excluir_Click(object sender, RoutedEventArgs e)
+        {
+            if (contatoslista.SelectedItem != null) contatoslista.Items.Remove(contatoslista.SelectedItem);
+        }
+    }
+}
---
+++ aulas/aula001/contato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula001
+{
+    internal class contato
+    {
+        private string n, t, e;
+        public contato(string nom, string tel, string ema)
+        {
+            if (nom != "") n = nom;
+            if (tel != "") t = tel;
+            if (ema != "") e = ema;
+        }
+        public override string ToString()
+        {
+            return $"{n} - {e} - {t}";
+        }
+    }
+}
