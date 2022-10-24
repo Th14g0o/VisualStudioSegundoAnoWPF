34bd08048b8d8db60460fad39c7dd0a62f752739
Thiago Gonsalves Da Silva <20211011110014@ifrn.local>
2022-10-24 18:06:37 -0300

    Aula 2 finalização

---
+++ aulas/Aula002/Tri.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula002
+{
+    class Tri
+    {
+        private double b, a;
+        public void SetBase(double x)
+        {
+            if (x > 0) b = x;
+        }
+        public void SetAltu(double y)
+        {
+            if (y > 0) a = y; 
+        }
+
+        public double GetBase()
+        {
+            return b;
+        }
+        public double GetAltu()
+        {
+            return a;
+        }
+
+        public double CalcArea()
+        {
+            return b * a;
+        }
+        public double CalcDiag()
+        {
+           return  Math.Sqrt(b * b + a * a);
+        }
+    }
+}
