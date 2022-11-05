cf8a8aac4a88c942e8d352e151f0e47a135860a4
Th14g0o <thiago.gonsalves@escolar.ifrn.edu.br>
2022-11-05 13:57:40 -0300

    Exercicio 5 da Lista4-AplicarWPF

---
+++ listas/POO-Lista4-AplicarWpf/Ex05/Numeros.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05
+{
+    internal class Numeros
+    {
+        private int inicio, fim;
+        public Numeros(int i, int f)
+        {
+            if (i < f)
+            {
+                inicio = i;
+                fim = f;
+            }
+            else
+            {
+                inicio = f;
+                fim = i;
+            }
+       
+        }
+        public int[] Calcular(bool? i, bool? p)
+        {
+
+            string ns = "";
+            int count = 0;
+
+            for (int valor = inicio; valor <= fim; valor++)
+            {
+                if (i == true && p == true)
+                {
+                    if (count > 0) ns += " ";
+                    ns += valor.ToString();
+                    count++;
+                }
+                else if (i == true && p == false)
+                {
+                    if (count > 0 && valor % 2 != 0) ns += " ";
+                    if (valor % 2 != 0) ns += valor.ToString();
+                    count++;
+                }
+                else
+                {
+                    if (count > 0 && valor % 2 == 0) ns += " ";
+                    if (valor % 2 == 0) ns += valor.ToString();
+                    count++;
+                }
+            }
+            count = 0;
+            string[] nums = ns.Split();
+            int[] interv = new int[nums.Length];
+            for (int indice = 0; indice < nums.Length; indice++)
+            {
+                interv[indice] = int.Parse(nums[indice]);
+            }
+            return interv;
+
+
+
+
+        }
+    }
+}
