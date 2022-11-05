1d197f078bea2037afb4d28637929a6a42e8eabe
Th14g0o <thiago.gonsalves@escolar.ifrn.edu.br>
2022-11-05 16:31:54 -0300

    Organizado

--- aulas/aula004/obj/Debug/net5.0-windows/App.g.i.cs
+++ aulas/aula004/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace aula004 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace aula004 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
         public static void Main() {
             aula004.App app = new aula004.App();
             app.InitializeComponent();
