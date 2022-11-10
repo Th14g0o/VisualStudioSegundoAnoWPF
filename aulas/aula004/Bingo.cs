881aea738761acd45395588cb7870cfc915a3f76
Th14g0o <thiago.gonsalves@escolar.ifrn.edu.br>
2022-11-10 01:22:47 -0300

    Aula004 atualizada mas com bug

---
+++ aulas/aula004/Bingo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula004
+{
+    internal class Bingo
+    {
+        private int numBolas, index;
+        private int[] Numeros;
+        public void Iniciar(int numBolas)
+        {
+            Random substituto = new Random();
+            if (numBolas >= 10 && numBolas <= 100)
+                this.numBolas = numBolas;
+            else
+                this.numBolas = substituto.Next(10, 101);
+            Numeros = new int[numBolas];
+            index = 0;
+        }
+        public int Proximo()
+        {
+            if (index == numBolas) return -1;
+            Random valor = new Random();
+
+            int bola = valor.Next(1, numBolas + 1);
+            int verifica = Array.IndexOf(Numeros, bola);
+
+            while (verifica != -1)
+            {
+                bola = valor.Next(10, numBolas + 1);
+                verifica = Array.IndexOf(Numeros, bola);
+            }
+            Numeros[index] = bola;
+            index++;
+            return bola;
+        }
+        public int[] Sorteados()
+        {
+            int[] vetor = new int[index];
+            Array.Copy(Numeros, vetor, index);
+            return vetor;
+        }
+    }
+}
--- aulas/aula004/MainWindow.xaml.cs
+++ aulas/aula004/MainWindow.xaml.cs
@@ -27,7 +27,31 @@ namespace aula004
 
         private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            bolas.Text = slider.Value.ToString();
+            bolas_ini.Text = slider.Value.ToString();
+        }
+
+        Bingo jogo = new Bingo();
+
+        private void inicio_Click(object sender, RoutedEventArgs e)
+        {
+            //Bingo jogo = new Bingo();
+            jogo.Iniciar(int.Parse(bolas_ini.Text));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Bingo jogo = new Bingo();
+            //jogo.Iniciar(int.Parse(bolas_ini.Text));
+            if (jogo.Proximo() == -1) sorteando.IsEnabled = false;
+            ultima_bola.Text = (jogo.Proximo()).ToString();
+            int tam = jogo.Sorteados().Length;
+            string bolas_usadas = "";
+            for (int indice = 0; indice < tam; indice++)
+            {
+                if (indice > 0) bolas_usadas += " - ";
+                bolas_usadas += ((jogo.Sorteados())[indice]).ToString();
+            }
+            usadas.Text = bolas_usadas;
         }
     }
 }
