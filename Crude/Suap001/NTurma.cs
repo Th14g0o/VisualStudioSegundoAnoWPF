bdac6c8b1577d6121f375730aabfec3f1eaa9f21
Thiago Gonsalves Da Silva <20211011110014@ifrn.local>
2022-12-30 16:11:27 -0300

    Crude Atuali 02

---
+++ Crude/Suap001/NTurma.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suap001
+{
+    static class NTurma
+    {
+        private static List<Turma> turmas = new List<Turma>();
+
+        public static void Inserir(Turma t)
+        {
+            Abrir();
+            turmas.Add(t);
+            Salvar();
+        }
+
+        public static List<Turma> Listar()
+        {
+            Abrir();
+            return turmas;
+        }
+
+        public static Turma IdT(int id)
+        {
+            foreach (Turma t in turmas)
+                if (t.Id == id) return t;
+            return null;
+        }
+
+        public static void Atualizar(Turma t)
+        {
+            Abrir();
+            Turma obj = IdT(t.Id);
+            obj.Curso = t.Curso;
+            obj.Descricao = t.Descricao;
+            obj.AnoLetivo = t.AnoLetivo;
+            Salvar();
+
+        }
+
+        public static void Excluir(Turma t)
+        {
+            Abrir();
+            turmas.Remove(IdT(t.Id));
+            Salvar();
+        }
+
+        public static void Abrir()
+        {
+            StreamReader f = null;
+            try // caso der para efutuar os comandos a seguir
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Turma>));
+                f = new StreamReader("./TurmasRegistro.xml");
+                turmas = (List<Turma>)xml.Deserialize(f);
+            }
+            catch// caso não der para efutuar os comandos acima
+            {
+                turmas = new List<Turma>();
+            }
+            if (f != null) f.Close();
+
+        }
+
+        public static void Salvar()
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(List<Turma>));
+            StreamWriter f = new StreamWriter("./TurmasRegistro.xml", false); 
+            xml.Serialize(f, turmas); 
+            f.Close();
+        }
+    }
+}
--- Crude/Suap001/TurmaWindow.xaml.cs
+++ Crude/Suap001/TurmaWindow.xaml.cs
@@ -23,5 +23,43 @@ namespace Suap001
         {
             InitializeComponent();
         }
+
+        private void InserirClick(object sender, RoutedEventArgs e)
+        {
+            Turma t = new Turma { Id = int.Parse(txtId.Text), Curso = txtCurso.Text, AnoLetivo = int.Parse(txtAno.Text), Descricao = txtTurma.Text };
+            NTurma.Inserir(t);
+            ListarClick(sender, e);// chama a ação do butão
+        }
+
+        private void ListarClick(object sender, RoutedEventArgs e)
+        {
+            listTurmas.ItemsSource = null;
+            listTurmas.ItemsSource = NTurma.Listar();
+            //listTurmas.Items.Clear();
+            //foreach (Turma t in NTurma.Listar())
+            //listTurmas.Items.Add(t);
+        }
+
+        private void AtualizarClick(object sender, RoutedEventArgs e)
+        {
+            Turma t = new Turma { Id = int.Parse(txtId.Text), Curso = txtCurso.Text, AnoLetivo = int.Parse(txtAno.Text), Descricao = txtTurma.Text };
+            NTurma.Atualizar(t);
+            ListarClick(sender, e);
+        }
+
+        private void ExcluirClick(object sender, RoutedEventArgs e)
+        {
+            //sender e o proprio butão e "e" serria os dados que vem
+            if (listTurmas.SelectedItem != null)
+            {
+                NTurma.Excluir((Turma)listTurmas.SelectedItem);//Nome_Classe entre parentes antes de algo tenta converter
+                ListarClick(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Selecione um item para deletar!");
+            }
+
+        }
     }
 }
